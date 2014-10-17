60a326c47d8f476edf83bdfa2800a46f8a21db3f
Olivier Laviale <contact@weirdog.com>
2014-10-17 17:52:53 +0200

    Added NodeTermCriterion

---
+++ lib/activerecords/facets/node-term.php
@@ -0,0 +1,55 @@
+<?php
+
+/*
+ * This file is part of the Icybee package.
+ *
+ * (c) Olivier Laviale <olivier.laviale@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Icybee\Modules\Taxonomy\Terms;
+
+use ICanBoogie\ActiveRecord\Criterion;
+use ICanBoogie\ActiveRecord\Query;
+use ICanBoogie\ActiveRecord;
+use ICanBoogie\generate_token;
+
+class NodeTermCriterion extends Criterion
+{
+	static private $tokens = [];
+
+	static protected function generate_token()
+	{
+		$possible = \ICanboogie\TOKEN_ALPHA;
+
+		for (;;)
+		{
+			$token = \ICanBoogie\generate_token(6, $possible);
+
+			if (empty($tokens[$token]))
+			{
+				return $tokens[$token] = $token;
+			}
+		}
+	}
+
+	public function alter_query_with_value(Query $query, $value)
+	{
+		$token = self::generate_token();
+		$v_alias = $token . '_v';
+		$t_alias = $token . '_t';
+
+		$q = ActiveRecord\get_model('taxonomy.vocabulary')
+		->select("nid, vocabularyslug AS $v_alias, termslug AS $t_alias")
+		->join(':taxonomy.terms')
+		->join(':taxonomy.terms/nodes');
+
+		$query
+		->join($q, [ 'on' => 'nid' ])
+		->where([ $v_alias => $this->id, $t_alias => $value]);
+
+		return $query;
+	}
+}
