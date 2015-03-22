ec1a6aeeed04d7ccd478304e150502b97cb258ef
Olivier Laviale <olivier.laviale@gmail.com>
2015-03-22 19:54:50 +0100

    Added TermCriterion

--- lib/activerecords/facets/term.php
+++ lib/activerecords/facets/term.php
@@ -11,9 +11,37 @@
 
 namespace Icybee\Modules\Taxonomy\Terms;
 
+use ICanBoogie\ActiveRecord\Query;
 use ICanBoogie\Facets\Criterion;
 
+/**
+ * A criterion for terms.
+ */
 class TermCriterion extends Criterion
 {
+	public function alter_query_with_value(Query $query, $value)
+	{
+		$vocabulary_slug = $this->id;
+		$constructor = $query->model->id;
 
+		if (substr($vocabulary_slug, -4, 4) === 'slug')
+		{
+			$vocabulary_slug = substr($vocabulary_slug, 0, -4);
+		}
+
+		$taxonomy_query = $query->model->models['taxonomy.vocabulary']
+			->join(':taxonomy.vocabulary/scopes')
+			->join(':taxonomy.terms')
+			->join(':taxonomy.terms/nodes')
+			->select('nid')
+			->and([
+
+				'termslug' => $value,
+				'vocabularyslug' => $vocabulary_slug,
+				'constructor' => $constructor
+
+			]);
+
+		return $query->filter_by_nid($taxonomy_query);
+	}
 }
